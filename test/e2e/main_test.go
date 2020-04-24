ef67cb71c5d14cf2155a7543a87a1f6e4385bdfb
Paul Gier <pgier@redhat.com>
2020-04-24 10:42:53 -0500

    test/e2e: add volume claim template metadata test
    Check that metadata added to a volume claim template in a prometheus
    resource is retained in both the Prometheus object and the stateful set.

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -185,6 +185,7 @@ func testAllNSPrometheus(t *testing.T) {
 		"PromNoServiceMonitorSelector":           testPromNoServiceMonitorSelector,
 		"PromVersionMigration":                   testPromVersionMigration,
 		"PromResourceUpdate":                     testPromResourceUpdate,
+		"PromStorageLabelsAnnotations":           testPromStorageLabelsAnnotations,
 		"PromStorageUpdate":                      testPromStorageUpdate,
 		"PromReloadConfig":                       testPromReloadConfig,
 		"PromAdditionalScrapeConfig":             testPromAdditionalScrapeConfig,
